6238404e85c88b8a314f97d0466cb6a9a044b465
jcarcamo <carcamo1cruz@gmail.com>
2025-11-23 14:22:01 -0600

    feat(chronicles): add Post model schema

--- chronicles/models.py
+++ chronicles/models.py
@@ -1,3 +1,31 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        PUBLISHED = "PB", "Published"
+        DRAFT = "DF", "Draft"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="chronicles_posts",
+    )
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+
+    def __str__(self):
+        return self.title
