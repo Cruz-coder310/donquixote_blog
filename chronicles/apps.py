2500b6ee1654c54568d997be172f30b95fe554cb
jcarcamo <carcamo1cruz@gmail.com>
2025-11-23 12:29:33 -0600

    ⚙️ chore: normalized quotes & removed autogenereted docstring

--- chronicles/apps.py
+++ chronicles/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ChroniclesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chronicles'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "chronicles"
