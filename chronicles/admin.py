a640192d1d26c7ac54cd9389773b6e33a4162dc6
jcarcamo <carcamo1cruz@gmail.com>
2025-11-23 16:52:04 -0600

    🦩 feat(admin): registered Post model with custom admin configuration

--- chronicles/admin.py
+++ chronicles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "publish"]
+    list_filter = ["status"]
+    prepopulated_fields = {"slug": ("title",)}
+    show_facets = admin.ShowFacets.ALWAYS
