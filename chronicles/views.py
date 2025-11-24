cbf46db3fc29410968ec005644b53c89e5dad8cd
jcarcamo <carcamo1cruz@gmail.com>
2025-11-24 12:31:22 -0600

    feat(chronicles): add slug-based post detail view

--- chronicles/views.py
+++ chronicles/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
 from .models import Post
 
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "chronicles/post_list.html", {"posts":posts})
+    return render(request, "chronicles/post_list.html", {"posts": posts})
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status=Post.Status.PUBLISHED)
+
+    return render(request, "chronicles/post_detail.html", {"post": post})
