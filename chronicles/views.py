f60a5f9e9114f72b35b0608fa7fce3560a1fd480
jcarcamo <carcamo1cruz@gmail.com>
2025-11-26 19:43:50 -0600

    🦩 feat(chronicles): added pagination to post_list view; created pagination.html & included it in post_list.html; created pyproject.toml for djlint configuration

--- chronicles/views.py
+++ chronicles/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import get_object_or_404, render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(request, "chronicles/post_list.html", {"posts": posts})
+    post_list = Post.published.all()
+    # Pagination with 3 post per page.
+    paginator = Paginator(post_list, 3)
+    page_num = request.GET.get("page", 1)
+
+    try:
+        page_obj = paginator.page(page_num)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+
+    return render(request, "chronicles/post_list.html", {"posts": page_obj})
 
 
 def post_detail(request, year, month, day, slug):
