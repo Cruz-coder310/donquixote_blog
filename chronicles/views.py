a61aa15c9af1e6505bc2a68ae578df974d9f3aea
jcarcamo <carcamo1cruz@gmail.com>
2025-12-22 23:56:41 -0600

    feat(chronicles): add email sharing functionality

--- chronicles/views.py
+++ chronicles/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import get_object_or_404, render
+# Django Core
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Count
+from django.core.mail import send_mail
 
+# Third-Party package
 from taggit.models import Tag
 
+# Local apps impots
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -58,5 +63,45 @@ def post_detail(request, year, month, day, slug):
         {
             "post": post,
             "similar_posts": similar_posts,
+        },
+    )
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post.published, id=post_id)
+    sent = False
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} te recomienda: {post.title}"
+
+            message = (
+                f"Hola,\n\n"
+                f"{cd['name']} ({cd['email']}) quiere compartir contigo este post:\n"
+                f"{post.title}\n{post_url}\n\n"
+                f"Comentarios:\n{cd['comments']}\n\n"
+                "Saludos,"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]],
+            )
+            sent = True
+
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        "chronicles/post_share.html",
+        {
+            "form": form,
+            "post": post,
+            "sent": sent,
         },
     )
