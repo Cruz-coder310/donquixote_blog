b0e695393d62ee04a1044327aed9877ba3d1578a
jcarcamo <carcamo1cruz@gmail.com>
2025-12-27 03:10:23 -0600

    feat: add post count template tag & apply in sidebar

--- chronicles/templatetags/chronicles_extras.py
+++ chronicles/templatetags/chronicles_extras.py
@@ -2,6 +2,8 @@ import markdown
 from django import template
 from django.utils.html import mark_safe
 
+from ..models import Post
+
 register = template.Library()
 
 
@@ -10,3 +12,9 @@ def markdown_format(text):
     # TODO: This is unsafe for user input. add sanitization (bleach) later.
     html = markdown.markdown(text)
     return mark_safe(html)
+
+
+@register.simple_tag
+def get_post_count():
+    post_count = Post.published.count()
+    return post_count
