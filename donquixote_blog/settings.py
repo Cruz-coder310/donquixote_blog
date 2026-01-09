18edfe0c2a92bbfffb9d97c005711a3b48aa5272
jcarcamo <carcamo1cruz@gmail.com>
2026-01-08 23:11:00 -0600

    feat(db): configure PosgreSQL connection & add initial dumpdata

--- donquixote_blog/settings.py
+++ donquixote_blog/settings.py
@@ -1,3 +1,4 @@
+import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 
@@ -72,10 +73,7 @@ WSGI_APPLICATION = "donquixote_blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.parse(config("DATABASE_URL")),
 }
 
 
