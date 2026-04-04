464ca98f69eb38c386470708182f8d71ede579c0
jcarcamo <carcamo1cruz@gmail.com>
2026-04-03 23:16:44 -0600

    feat(auth): add logout support with dashboard

--- donquixote_blog/urls.py
+++ donquixote_blog/urls.py
@@ -4,11 +4,19 @@ from django.contrib.sitemaps.views import sitemap
 
 from chronicles.sitemaps import PostSitemap
 
+# This view helps me to check LOGOUT_REDIRECT_URL setting
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html", {})
+
 
 sitemaps = {"posts": PostSitemap}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", home, name="home"),
     path("chronicles/", include("chronicles.urls")),
     path("accounts/", include("accounts.urls")),
     path(
